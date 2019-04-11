8f23a928c3bc38d664698f23f5007a9fbda5e3b8
TTLExpired <bill.mosses@gmail.com>
2019-04-11 18:40:26 +1000

    More refactoring

--- misc/bgp_data.py
+++ misc/bgp_data.py
@@ -5,14 +5,16 @@ def main():
         '''Get some bgp data and save to a csv file'''
 
         # First, get list of devices
-        devices = ['10.47.254.252', '10.47.254.251',
-                   '10.0.7.254', '10.0.7.253']
+        devices = ['10.47.254.252',
+                   '10.47.254.251',
+                   '10.0.7.254',
+                   '10.0.7.253']
 
         # iosxr driver
         xr_driver = get_network_driver('iosxr')
         ios_driver = get_network_driver('ios')
 
-        # Some variables
+        # Some easier to remember keys
         remote_as = 'remote_as'
         router_id = 'router_id'
         state = 'connection_state'
@@ -34,23 +36,16 @@ def main():
                                 bgp_data = wan.get_bgp_neighbors_detail()
                                 list_bgp_data.append(bgp_data)
 
+        # Make up a csv header and save it.
         header = 'router_id,remote_as,received_routes,transmit_routes,state,flap'
 
         with open('bgpdata.csv', 'a') as f:
                 f.write(header + '\n')
 
-        print(header)
         for bgp_data in list_bgp_data:
                 for key, value in bgp_data.items():
                         for nkey, nvalue in value.items():
                                 for data in nvalue:
-                                        # print(str(data[router_id]) + ',' +
-                                        #       str(data[remote_as]) + ',' +
-                                        #       str(data[rx_route]) + ',' +
-                                        #       str(data[tx_route]) + ',' +
-                                        #       str(data[state]) + ',' +
-                                        #       str(data[flap]))
-
                                         with open('bgpdata.csv', 'a') as f:
                                                 f.write(str(data[router_id]) + ',' +
                                                         str(data[remote_as]) + ',' +
