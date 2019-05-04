f17aa9bb4c43a3397a334c2a23e0ec0f7f926ccf
TTLExpired <bill.mosses@gmail.com>
2019-05-04 11:38:12 +1000

    First Flask/Form personal Study

---
+++ flask/myflask_testing/form1.py
@@ -0,0 +1,27 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'MyLittleSecret'
+
+
+class LoginForm(FlaskForm):
+    ''' This is our class form. It inherits from flask_wtf FlaskForm '''
+
+    # wtform is our main engine:
+    username = StringField('username')
+    password = PasswordField('password')
+
+@app.route('/form', methods=['GET', 'POST'])
+def form():
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        return f'Thank you {form.username.data}'
+    # Pass the completed form to the template!
+    return render_template('form.html', form=form)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
